0823af3d1489420b8be042b0f33161af19d7f6ae
jz6277 <jz6277@nyu.edu>
2025-02-22 22:52:10 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,18 +17,26 @@ module tt_um_group1 (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-    integer i;
     wire [15:0] In;  // 16-bit input combining ui_in and uio_in
     assign In = {ui_in, uio_in};  // Concatenate ui_in as upper and uio_in as lower bits
 
-    always @(*) begin
-        uo_out = 8'b1111_0000;  // Default output if all bits are 0
-        for (i = 15; i >= 0; i = i - 1) begin
-            if (In[i] && uo_out == 8'b1111_0000) begin
-                uo_out = i;  // Store index of first '1' found
-            end
-        end
-    end
+    assign uo_out = (In[15]) ? 8'd15 :
+                    (In[14]) ? 8'd14 :
+                    (In[13]) ? 8'd13 :
+                    (In[12]) ? 8'd12 :
+                    (In[11]) ? 8'd11 :
+                    (In[10]) ? 8'd10 :
+                    (In[9])  ? 8'd9  :
+                    (In[8])  ? 8'd8  :
+                    (In[7])  ? 8'd7  :
+                    (In[6])  ? 8'd6  :
+                    (In[5])  ? 8'd5  :
+                    (In[4])  ? 8'd4  :
+                    (In[3])  ? 8'd3  :
+                    (In[2])  ? 8'd2  :
+                    (In[1])  ? 8'd1  :
+                    (In[0])  ? 8'd0  :
+                               8'b1111_0000;  // Default case when all bits are 0
   assign uio_out = 0;
   assign uio_oe  = 0;
 
